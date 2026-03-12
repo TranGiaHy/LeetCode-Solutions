2fc9fe4f0a29a04fc5e14a7d1eacaf9e758de3e4
TranGiaHy <151387306+TranGiaHy@users.noreply.github.com>
2026-03-12 22:10:13 +0700

    Time: 0 ms (100%), Space: 12.4 MB (51.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -12,15 +12,25 @@ class Solution(object):
         k = m + n - 1
 
         while i >= 0 and j >= 0:
+            # Nếu giá trị tại i > giá trị tại j
             if nums1[i] > nums2[j]:
+                # Đưa giá trị của mảng 1 vào vị trí thứ k
                 nums1[k] = nums1[i]
+                # i lùi về để xét giá trị tiếp theo của mảng 1
                 i -= 1
             else:
-                nums1[k] = nums2[j]
+                # Đưa giá trị của mảng 2 vào vị trí thứ k
+                nums1[k] = nums2[j] 
+                # j lùi về để xét giá trị tiếp theo của mảng 2
                 j -= 1
+            
+            # Lấp xong 1 ô trống thì lùi k về để lấp ô tiếp theo
             k -= 1
-
+        
+        # Trường hợp nếu mảng 1 đã được xét hết phần tử trước
         while j >= 0:
+            # Đưa nốt các phần tử còn thừa từ mảng 2 vào mảng 1
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
+        
