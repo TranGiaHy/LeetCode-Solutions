cabad5732568540b8d1020d9425524220d485e19
TranGiaHy <151387306+TranGiaHy@users.noreply.github.com>
2026-03-13 16:12:05 +0700

    Time: 0 ms (100%), Space: 12.3 MB (83.54%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        # Số kẹo lớn nhất hiện có trong danh sách ( Kẹo của trẻ có nhiều nhất )
+        max_candy = max(candies)
+
+        # Tạo danh sách rỗng
+        result = []
+        
+        # Duyệt qua từng đứa trẻ (candy đại diện cho số kẹo của 1 đứa)
+        for candy in candies:
+            # ktra Nếu số kẹo hiện tại + số kẹo thêm >= Kẹo của trẻ có nhiều nhất
+            if candy + extraCandies >= max_candy:
+                # Thêm true vào ds nếu >= Kẹo của trẻ có nhiều nhất
+                result.append(True)
+            else:
+                # Thêm false vào ds nếu <= Kẹo của trẻ có nhiều nhất
+                result.append(False)
+
+        # trả về kqua
+        return result
+
+        
